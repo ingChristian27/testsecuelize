2c7594c9985f00a08754ac07966eee46ad7036d9
Diego Martinez <ddmtzb>
2018-08-15 12:28:42 -0300

    Adding apidocs samples responses

--- server/router/drivpass.js
+++ server/router/drivpass.js
@@ -44,6 +44,28 @@ router
     *
     * @apiSuccess {String} firstname Firstname of the User.
     * @apiSuccess {String} lastname  Lastname of the User.
+    * @apiSuccessExample Success-Response:
+    *     HTTP/1.1 200 OK
+    *       {
+    *         success: true,
+    *         message: "User created!",
+    *         insertId: post.id,
+    *         access_token: acces token
+    *       }
+    * 
+    * @apiSuccessExample Success-Response:
+    *     HTTP/1.1 203 OK
+    *     {
+    *       "message": "This email is already registered"
+    *     }
+    * 
+    * @apiError UserNotFound The id of the User was not found.
+    *
+    * @apiErrorExample Error-Response:
+    *   HTTP/1.1 404 Not Found
+    *     {
+    *       "error": "UserNotFound"
+    *     }
   */
   .post("/add", async (req, res, next) => {
     try {
