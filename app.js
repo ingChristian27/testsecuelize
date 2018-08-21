f518b09c35793b38931df81e84b1ed9894dad0a9
Christian Dachiardi <christiandachiardi@MacBook-Pro-de-Christian.local>
2018-08-21 12:44:01 -0500

    refactory appjs

--- app.js
+++ app.js
@@ -1,46 +1,14 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-//var jwt = require("jwt-simple");
 var auth = require("./server/config/auth.js")();
-var users = require("./server/config/users");
-//var cfg = require("./server/config/passport");
 
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(auth.initialize());
 app.use("/apidoc", express.static("apidoc"));
 require("./routes")(app);
-
-app.post("/token", function(req, res) {
-  if (req.body.email && req.body.password) {
-    var email = req.body.email;
-    var password = req.body.password;
-    var user = users.find(function(u) {
-      return u.email === email && u.password === password;
-    });
-    if (user) {
-      var payload = {
-        id: user.id
-      };
-      console.log(payload);
-      var token = jwt.encode(payload, cfg.jwtSecret);
-      //const token = jwt.sign(payload, cfg.jwtSecret, { expiresIn });
-      res.json({
-        token: token
-      });
-    } else {
-      res.sendStatus(401);
-    }
-  } else {
-    res.sendStatus(401);
-  }
-});
-
-app.get("/user", auth.authenticate(), function(req, res) {
-  res.json({ message: "Por fin paso esa berga!!" });
-});
 
 const port = parseInt(process.env.PORT, 10) || 8000;
 app.listen(port, function() {
