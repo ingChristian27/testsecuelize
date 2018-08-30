2a164956e7dae214c69e96c77553fef72dc5b77e
Christian Dachiardi <christiandachiardi@MacBook-Pro-de-Christian.local>
2018-08-30 13:54:34 -0500

    carga de imagenes

--- server/router/resource.js
+++ server/router/resource.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const resource = require("../controllers/resource");
+var multipart = require("connect-multiparty");
+var multipartMiddleware = multipart();
 
 var auth = require("../config/auth.js")();
 
-router.post("/add", auth.authenticate(), resource.add);
+router.post("/add", auth.authenticate(), multipartMiddleware, resource.add);
 
 module.exports = router;
