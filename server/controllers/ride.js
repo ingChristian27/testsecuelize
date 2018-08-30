fdda9d4e8c8f6b749d5bf822ac9cf5570967d6e4
Diego Martinez <ddmtzb>
2018-08-30 09:41:07 -0300

    Correcciones ride-add

--- server/controllers/ride.js
+++ server/controllers/ride.js
@@ -3,10 +3,13 @@ const DatabaseRideDriver = require("../database/ride_driver");
 const DatabaseDrivpass = require("../database/drivpass");
 const DatabaseCarInfo = require("../database/car_info");
 
+const moment = require("moment");
+
 async function add(req, res) {
   try {
     const params = req.body;
     params.id_driver_type = 1;
+    params.status = 1;
     const ride = await Database.selectByPassenger(params.id_passenger);
     if (ride == null) {
       return res
@@ -18,13 +21,16 @@ async function add(req, res) {
     }
     //    if(ride.rows.length > 0)  return res.status(200).json({details: "You already have a ride in progress!"});
     const post = await Database.insert(params);
-    const rideResult = await Database.selectById(post.id);
+    console.log('------------------------------------');
+    console.log("post",post);
+    console.log('------------------------------------');
+    //const rideResult = await Database.selectById(post.id);
 
     return res
       .status(200)
       .json({
         success: true,
-        Data: rideResult.rows[0],
+        Data: post,
         currentDate: moment(new Date())
       });
   } catch (e) {
